7f4b3b0f0f301210f94424e435174fd86280dac9
cd-k0nstant1n <116379151+cd-k0nstant1n@users.noreply.github.com>
2023-02-13 17:33:16 +0200

    password validation
    if the 2 passwords that are in a form do not match you can't register

--- Forms/scripts.js
+++ Forms/scripts.js
@@ -5,6 +5,7 @@ function yes_check(){
       }
       else document.getElementById('ifYes').style.display = 'none';
   }
+  
   function roleCheck() {
     if (document.getElementById('dot-1').checked) {
       fetch("../registerForms/studentForm.html").then(response=> response.text()).then(text=> document.getElementById('Form').innerHTML = text);
@@ -14,5 +15,17 @@ function yes_check(){
     }
     else if(document.getElementById('dot-3').checked){
     fetch("../registerForms/parentForm.html").then(response=> response.text()).then(text=> document.getElementById('Form').innerHTML = text);
+    }
+  }
+
+  function validate() {
+    var password1 = document.getElementById("pass1").value;
+    var password2 = document.getElementById("pass2").value;
+    if (password1 != password2) {
+      document.getElementById("error").innerHTML = "Passwords do not match.";
+      return false;
+    } else {
+      document.getElementById("error").innerHTML = "";
+      return true;
     }
   }
