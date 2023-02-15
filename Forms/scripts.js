77cad3ef5e3f4687ec5f9e852bd5e20484135f10
cd-k0nstant1n <116379151+cd-k0nstant1n@users.noreply.github.com>
2023-02-15 13:10:57 +0200

    register forms fixed

--- Forms/scripts.js
+++ Forms/scripts.js
@@ -34,19 +34,19 @@ function yes_check(){
   }
 
 
-        const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
+        // const togglePassword = document.querySelector("#togglePassword");
+        // const password = document.querySelector("#password");
 
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
+        // togglePassword.addEventListener("click", function () {
+        //     // toggle the type attribute
+        //     const type = password.getAttribute("type") === "password" ? "text" : "password";
+        //     password.setAttribute("type", type);
             
-            // toggle the icon
-            this.classList.toggle("bi-eye");
-        });
-            // prevent form submit
-            const form = document.querySelector("form");
-            form.addEventListener('submit', function (e) {
-                e.preventDefault();
-            });
+        //     // toggle the icon
+        //     this.classList.toggle("bi-eye");
+        // });
+        //     // prevent form submit
+        //     const form = document.querySelector("form");
+        //     form.addEventListener('submit', function (e) {
+        //         e.preventDefault();
+        //     });
