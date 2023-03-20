929a088959782f2e2aeccdb65bb35e98ee455087
cd-k0nstant1n <116379151+cd-k0nstant1n@users.noreply.github.com>
2023-03-21 00:07:07 +0200

    mainly home changes
    - nav bar ready
    - added welcome page(we won't use it )

--- Home/Home.php
+++ Home/Home.php
@@ -1,15 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="bg">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="Home.css">
-	<title>Home</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<title>Начало</title>
 </head>
 <body>
-	<div class="logo">
-		<h1>E-dnevnikbg</h1>
-	</div>
-</body>
+	<header>
+		<div class="navbar">
+			<div class="logo"><a href="#">e-dnevnikbg</a></div>
+			<ul class="links">
+				<li><a href="Home.php">Начало</a></li>
+				<li><a href="#">Дневник</a></li>
+				<li><a href="#">Програма</a></li>
+				<li><a href="#">Отзиви</a></li>
+				<li><a href="#">Събития</a></li>
+				<li><a href="#">За нас</a></li>
+			</ul>
+			<a href="../Forms/LoginForm/Login_form.html" class="action_btn">Влез</a>
+			<div class="toggle_btn">
+				<i class="fa-solid fa-bars"></i>
+			</div>
+		</div>
+
+		<div class="dropdown_menu">
+			<li><a href="Home.php">Начало</a></li>
+			<li><a href="#">Дневник</a></li>
+			<li><a href="#">Програма</a></li>
+			<li><a href="#">Отзиви</a></li>
+			<li><a href="#">Събития</a></li>
+			<li><a href="#">За нас</a></li>
+			<li><a href="#" class="action_btn">Влез</a></li>
+		</div>
+	</header>
+
+	<main>
+		
+	</main>
+
+	<script src="../scripts.js"></script>
+</body>	
 </html>
