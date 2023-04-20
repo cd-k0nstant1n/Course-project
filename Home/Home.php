956f796e4ed8272be7c69cc75f34632e87ca7e2a
IvayloDZH05 <116313439+IvayloDZH05@users.noreply.github.com>
2023-04-20 21:23:55 +0300

    New logo ; Profile button/menu changed

--- Home/Home.php
+++ Home/Home.php
@@ -4,14 +4,14 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="Home.css">
+	<link rel="stylesheet" href="KUR.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<title>Начало</title>
 </head>
 <body>
 	<header>
 		<div class="navbar">
-			<div class="logo"><a href="#">e-dnevnikbg</a></div>
+			<img src="../images/Project-Logo-2.png" class="logo">
 			<ul class="links">
 				<li><a href="Home.php">Начало</a></li>
 				<li><a href="Journal.php">Дневник</a></li>
@@ -32,10 +32,27 @@
 					echo '<div class="profile-box">
 					<button class="profile-btn">Профил</button>
 					<ul class="profile-dropdown">
-					  <li><a href="#">Моят профил</a></li>
-					  <li><a href="../Forms/LoginForm/Login_form.php">Излез</a></li>
-					</ul>
-				  </div>';
+					<div class="sub-menu">
+					  <li><a href="MyProfile.php" class="sub-menu-link">
+						     <img src="../images/profile.png">
+								 <p>Моят Профил</p>
+								 <span>></span>
+						</li></a>	
+
+					  <li><a href="#" class="sub-menu-link">
+						<img src="../images/setting.png">
+						<p>Настройки</p>
+						<span>></span>
+			      </a></li>
+
+						<li><a href="../Forms/LoginForm/Login_form.php" class="sub-menu-link">
+						     <img src="../images/logout.png">
+								 <p>Изход</p>
+								 <span>></span>
+						</li></a>						
+				  </ul>
+				  </ul>
+					';
 				}
 			?>
 			<div class="toggle_btn">
