91b6d96b68666e084b275793217b005ccece65b7
martiblq1 <124778787+martiblq1@users.noreply.github.com>
2023-06-10 13:28:45 +0300

    Forgotten password

--- Forms/ForgottenPassword/ForgottenPassword.php
+++ Forms/ForgottenPassword/ForgottenPassword.php
@@ -1,13 +1,24 @@
 
 <?php
 session_start();
-
+include '../../Home/db_connection.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 20; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
 
 try{
 if (isset($_POST['submit'])) {
@@ -28,15 +39,49 @@ if (isset($_POST['submit'])) {
     $mail->isHTML(true);
 
     $mail->Subject = "Forgotten password in E-dnevnikbg";
-    $mail->Body = " ";
+	$newpass = randomPassword();
+    $mail->Body = "Use this password to log in and make a new one: " . $newpass;
 
     $mail->send();
-
-    echo "
-      <script>
-      alert('The email has been sent successfully.');
-      </script>
-    ";
+	$newpass = password_hash($newpass, PASSWORD_BCRYPT);
+	$newpass = mysqli_real_escape_string($connection, $newpass);
+	$usermail = $_POST["mail"];
+	
+	$sql = "SELECT * FROM students WHERE mail='$usermail'";
+	$result = mysqli_query($connection, $sql);
+	$tableName = "none";
+	
+	if(mysqli_num_rows($result) > 0)
+	{
+		$tableName = "students";
+	}
+	else
+	{
+		$sql = "SELECT * FROM parents WHERE mail='$usermail'";
+		$result = mysqli_query($connection, $sql);
+		
+		if(mysqli_num_rows($result) > 0)
+		{
+			$tableName = "parents";
+		}
+		else
+		{
+			$sql = "SELECT * FROM teachers WHERE mail='$usermail'";
+			$result = mysqli_query($connection, $sql);
+			
+			if(mysqli_num_rows($result) > 0)
+			{
+				$tableName = "teachers";
+			}
+		}
+	}
+	
+	if($tableName != "none")
+	{
+		$sql = "UPDATE " . $tableName . " SET password = '" . $newpass . "' WHERE mail = '" . $_POST["mail"] . "'";
+		$result = mysqli_query($connection, $sql);
+		header("Location: ../LoginForm/Login_form.php");
+	}
   }
 }catch(Exception $e){
   echo "error";
