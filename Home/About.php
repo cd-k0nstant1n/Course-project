3cea06853855152dcdf12068ccb11df72a6d368f
IvayloDZH05 <116313439+IvayloDZH05@users.noreply.github.com>
2023-06-16 22:37:56 +0300

    About us page-finished version

--- Home/About.php
+++ Home/About.php
@@ -21,7 +21,8 @@
   <p>С развитиете на технологийте всичко това ще остане в миналото.Нашият екип разработи бърз и надежден начин за въвеждането и за известяването за оценка,отсъствие,похвала или бъдещо събитие във вашето училище! Чрез "E-dnevnik" всичко става с едно кликване на вашето устройство и единствено изискване,наличието на интернет!</p>
 	</div>
 
-  <div id="map-container">
+  <div class="container_all">
+  <div class="map-container">
     <div class="map-text">Тук може да ни намерите</div>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4133.503702566782!2d23.909577344486824!3d42.89756503949968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa501bf6a08c81%3A0xe4f50f5955f4a188!2z0J3Qn9CTINC_0L4g0LrQvtC80L_RjtGC0YrRgNC90Lgg0YLQtdGF0L3QvtC70L7Qs9C40Lgg0Lgg0YHQuNGB0YLQtdC80Lg!5e0!3m2!1sbg!2sbg!4v1686347575663!5m2!1sbg!2sbg"
             width="600"
@@ -30,18 +31,19 @@
             allowfullscreen=""
             loading="lazy">
     </iframe>
+    </div>
+  <div class="container-info">
+        <h2>Тук може да се свържете с нас</h2>
+        <p>Мартин Йонков</p>
+        <a href="mailto:19218@uktc-bg.com">19218@uktc-bg.com</a>
+        <p>Константин Петков</p>
+        <a href="mailto:19216@uktc-bg.com">19216@uktc-bg.com</a>
+        <p>Ивайло Джамбазов</p>
+        <a href="mailto:19212@uktc-bg.com">19212@uktc-bg.com</a>
+      </div>
   </div>
+</div>
 
-  <div class="contactBlock infoBlock">
-            <div class="infoWrap">
-                <div class="title">Meet us</div>
-                <ul>
-                    <li class="phone"><a href="tel:+40724343949">+40724343949</a></li>
-                    <li class="mail"><a href="mailto:contact@brandaffair.ro">contact@brandaffair.ro</a></li>
-                    <li class="adr"><a href="https://goo.gl/maps/UTZhQmUjPrM2" target="_blank">Amman St, no 35,  4th floor, ap 10, Bucharest</a></li>
-                </ul>
-            </div>
-        </div>
 	<?php include 'footer.php'  ?>
 
 	<script src="../scripts.js"></script>
--- sql/latest-remastered.sql
+++ sql/latest-remastered.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
---
--- Host: localhost    Database: journal_schema
--- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.22.04.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `events`
---
-
 DROP TABLE IF EXISTS `events`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `events` (
   `Name` varchar(100) DEFAULT NULL,
   `Beginning` varchar(100) DEFAULT NULL,
@@ -29,16 +6,9 @@ CREATE TABLE `events` (
   `Status` varchar(100) DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `parents`
---
+);
 
 DROP TABLE IF EXISTS `parents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `parents` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -49,16 +19,9 @@ CREATE TABLE `parents` (
   `phone` varchar(100) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `students`
---
+);
 
 DROP TABLE IF EXISTS `students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `students` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -91,16 +54,9 @@ CREATE TABLE `students` (
   `Programming` varchar(45) DEFAULT NULL,
   `Programming_absences` varchar(100) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `subjects`
---
+);
 
 DROP TABLE IF EXISTS `subjects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `subjects` (
   `id` int NOT NULL AUTO_INCREMENT,
   `class` varchar(100) DEFAULT NULL,
@@ -111,16 +67,9 @@ CREATE TABLE `subjects` (
   `Physical Education(PE)` varchar(100) DEFAULT NULL,
   `Music` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `teachers`
---
+) ;
 
 DROP TABLE IF EXISTS `teachers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `teachers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -131,16 +80,4 @@ CREATE TABLE `teachers` (
   `subject` varchar(45) DEFAULT NULL,
   `class_teacher` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-15 23:43:02
+);
